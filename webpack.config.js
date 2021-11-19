7ce4deaf20ead1ffe6b0c64c1a6cd2e0e0a9f121
mellowmorgan <morgan.waites@gmail.com>
2021-11-18 21:59:40 -0800

    add env

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
-
+const Dotenv = require('dotenv-webpack');
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -14,6 +14,7 @@ module.exports = {
   },
 
   plugins: [
+    new Dotenv(),
     new CleanWebpackPlugin(), 
     new HtmlWebpackPlugin({
       title: 'journal-entry-project',
